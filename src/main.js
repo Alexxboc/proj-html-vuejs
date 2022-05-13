6d9c6b08cb7e0f98b93967b0f5739335d8f5e21f
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-13 12:13:17 +0200

    Import google font Poppins

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "@fontsource/poppins"
+import "@fontsource/poppins/200.css"
+import "@fontsource/poppins/300.css"
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
+import "@fontsource/poppins/800.css"
+import "@fontsource/poppins/900.css"
+
+
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 
