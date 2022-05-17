c9ee03f9b3d79dc77819ac6e7abf2fa9cbada26f
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-17 10:09:14 +0200

    Create Footer copyright component

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,48 +1,20 @@
 <template>
   <footer>
     <FooterMenu />
-    <div class="copyrights p-3">
-      <div class="container">
-        <div class="row row-cols-2">
-          <div class="col p-3">
-            <p>Enjoy the low price. We are tracking any intention of piracy.</p>
-          </div>
-          <!-- /.col -->
-          <div class="col p-3">
-            <p class="text-end">
-              &#169; 2020 <span class="text-uppercase">nexgen</span> is Proudly
-              Powered by <span class="primary">Codings</span>.
-            </p>
-          </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row row-cols-2 -->
-      </div>
-      <!-- /.container -->
-    </div>
-    <!-- /.copyrights -->
+    <FooterCopyright />
+   
   </footer>
 </template>
 
 <script>
 import FooterMenu from "../Footer/FooterMenuComponent.vue";
+import FooterCopyright from "../Footer/FooterCopyrightComponent.vue"
 
 export default {
   name: "SiteFooter",
   components: {
     FooterMenu,
+    FooterCopyright
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.copyrights {
-  background-color: $consulting-copyright-bg;
-  color: $consulting-white-smoke;
-  font-size: 14px;
-  .primary {
-    color: $consulting-primary;
-    font-weight: 600;
-  }
-}
-</style>
